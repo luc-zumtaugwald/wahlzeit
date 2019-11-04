de65038aaaa62e4419a4f982ca5f3e71c7da58df
Luc Zumtaugwald <luc-suisse@gmx.de>
2019-11-04 14:20:46 +0100

    corrected Test Suite structure

--- src/test/java/org/wahlzeit/AllTests.java
+++ src/test/java/org/wahlzeit/AllTests.java
@@ -1,35 +1,19 @@
 package org.wahlzeit;
 
-import org.junit.runner.*;
-import org.junit.runners.*;
-import org.wahlzeit.handlers.TellFriendTest;
-import org.wahlzeit.model.*;
-import org.wahlzeit.model.persistence.*;
-import org.wahlzeit.services.*;
-import org.wahlzeit.services.mailing.*;
-import org.wahlzeit.utils.StringUtilTest;
-import org.wahlzeit.utils.VersionTest;
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.wahlzeit.handlers.HandlersTestSuite;
+import org.wahlzeit.model.ModelTestSuite;
+import org.wahlzeit.services.ServicesTestSuite;
+import org.wahlzeit.utils.UtilsTestSuite;
 
 @RunWith(Suite.class)
 @Suite.SuiteClasses({
-    TellFriendTest.class,
-    DatastoreAdapterTest.class,
-    AccessRightsTest.class,
-    CoordinateTest.class,
-    FlagReasonTest.class,
-    GenderTest.class,
-    GuestTest.class,
-    PhotoFilterTest.class,
-    TagsTest.class,
-    UserStatusTest.class,
-    ValueTest.class,
-    EmailServiceTest.class,
-    EmailAddressTest.class,
-    LogBuilderTest.class,
-    StringUtilTest.class,
-    VersionTest.class
+        HandlersTestSuite.class,
+        ModelTestSuite.class,
+        ServicesTestSuite.class,
+        UtilsTestSuite.class
 })
 public class AllTests{
-
 }
 
